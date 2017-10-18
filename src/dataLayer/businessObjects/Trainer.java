ca6e86b98f29c33ed7dab8fef6836890ac7493e5
KimWiese90 <kimwiese1905@gmx.de>
2017-10-18 11:27:14 +0200

    Update Trainer.java
    Erste Initialisierung, muss wahrscheinlich aber noch mal überarbeitet werden.

--- src/dataLayer/businessObjects/Trainer.java
+++ src/dataLayer/businessObjects/Trainer.java
@@ -1,14 +1,50 @@
 package dataLayer.businessObjects;
 
-public class Trainer {
+import businessObjects.ITrainer;
 
-	public Trainer() {
-		// TODO Auto-generated constructor stub
+public class Trainer implements ITrainer{
+	private int id;
+	private String name;
+	private int alter;
+	private int erfahrung;
+	
+	Trainer(int id, String name, int alter, int erfahrung){
+		this.id = id;
+		this.name = name;
+		this.alter = alter;
+		this.erfahrung = erfahrung;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int getId() {
+		return id;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAlter() {
+		return alter;
+	}
+
+	public void setAlter(int alter) {
+		this.alter = alter;
+	}
+
+	public int getErfahrung() {
+		return erfahrung;
+	}
+
+	public void setErfahrung(int erfahrung) {
+		this.erfahrung = erfahrung;
+		
+	}
 }
