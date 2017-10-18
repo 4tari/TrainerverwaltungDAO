51792d262d07fb302ff044e79f7dc7bf0c3dc024
KimWiese90 <kimwiese1905@gmx.de>
2017-10-18 14:37:46 +0200

    Update ITrainer.java
    Initial commit

--- src/dataLayer/dataAccessObjects/ITrainerDao.java
+++ src/dataLayer/dataAccessObjects/ITrainerDao.java
@@ -1,5 +1,18 @@
-package dataLayer.dataAccessObjects;
+package dataLayer.dataAcessObjects;
+
+import java.util.List;
+
+import businessObjects.ITrainer;
 
 public interface ITrainerDao {
-
+	public ITrainer create();
+	public void delete(ITrainer trainer);
+	public ITrainer first();
+	public ITrainer last();
+	public ITrainer next(ITrainer trainer);
+	public ITrainer previous(ITrainer trainer);
+	public void save(ITrainer trainer);
+	public List<ITrainer> select();
+	public ITrainer select(int id);
 }
+
