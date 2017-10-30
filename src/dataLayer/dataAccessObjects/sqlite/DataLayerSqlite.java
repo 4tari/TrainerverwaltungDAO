ed1f5cf250fad4bd12b833f1e061bc5095fdf542
KimWiese90 <kimwiese1905@gmx.de>
2017-10-30 18:55:17 +0100

    Update DataLayerSqlite.java

--- src/dataLayer/dataAccessObjects/sqlite/DataLayerSqlite.java
+++ src/dataLayer/dataAccessObjects/sqlite/DataLayerSqlite.java
@@ -1,14 +1,89 @@
-package dataLayer.dataAccessObjects.sqlite;
+package dataLayer.dataAcessObjects.sqlite;
 
-public class DataLayerSqlite {
+import java.awt.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import dataLayer.dataAcessObjects.ITrainerDao;
+import dataLayer.IDataLayer;
+import dataLayer.businessObjects.Trainer;
+
+
+public class DataLayerSqlite implements IDataLayer
+ {
+	public Connection conn = null;
+	
 	public DataLayerSqlite() {
-		// TODO Auto-generated constructor stub
-	}
 
-	public static void main(String[] args) {
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:sample.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            
+
+           // connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+
+            Statement statement = conn.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+
+//            statement.executeUpdate("DROP TABLE IF EXISTS person");
+//            statement.executeUpdate("CREATE TABLE person ('id' INTEGER PRIMARY KEY, 'name' STRING, 'alter' INTEGER, 'erfahrung' INTEGER)");
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+	}
+	
+	
+
+	public ITrainerDao getTrainerDao() {
 		// TODO Auto-generated method stub
 
-	}
+		Statement stmt;
+		try {
+			stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+			String sql = "SELECT * FROM Trainers";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			ArrayList<Trainer> trainers = new ArrayList<Trainer>();
+			while(rs.next())
+			{
+				Trainer trainer = new Trainer(rs.getInt("id"), rs.getString("name"), rs.getInt("alter"), rs.getInt("erfahrung"));
+				trainers.add(trainer);
+			}
+//			return trainers;
+			return null;
 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		String sql = "SELECT * FROM Trainers";
+//		rs = conn.executeQuery(sql);
+		
+		return null;
+	}
+	
+	
+	//public static void main(String[] args) throws ClassNotFoundException
+	//{
+	//}
 }
