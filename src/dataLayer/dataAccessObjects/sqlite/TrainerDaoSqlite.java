8be48f4d7ee91281d3fcb94546c5ad4f47ac186f
lexow1337 <32059398+lexow1337@users.noreply.github.com>
2017-11-02 13:09:17 +0100

    Update TrainerDaoSqlite.java

--- src/dataLayer/dataAccessObjects/sqlite/TrainerDaoSqlite.java
+++ src/dataLayer/dataAccessObjects/sqlite/TrainerDaoSqlite.java
@@ -1,7 +1,9 @@
 package dataLayer.dataAccessObjects.sqlite;
 
+import java.awt.HeadlessException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -14,66 +16,87 @@ import dataLayer.dataAccessObjects.ITrainerDao;
 public class TrainerDaoSqlite implements ITrainerDao {
 
 	public Connection conn = null;
-	private Connection doConnect(){
-		
-		 // db parameters
-        String url = "jdbc:sqlite:trainer.db";
-        // create a connection to the database
-        try {
+
+	private void doConnect() {
+
+		String url = "jdbc:sqlite:trainer.db";
+		try {
 			conn = DriverManager.getConnection(url);
 			Statement statement = conn.createStatement();
-	        statement.setQueryTimeout(30);  // set timeout to 30 sec.
-	        System.out.println("Connection to SQLite has been established.");
-	        return conn;
+			statement.setQueryTimeout(30); // set timeout to 30 sec.
+			System.out.println("Connection to SQLite has been established.");
+			// return conn;
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return null;
+			// return null;
 		}
-//        statement.executeUpdate("DROP TABLE IF EXISTS person");
-//        statement.executeUpdate("CREATE TABLE person ('id' INTEGER PRIMARY KEY, 'name' STRING, 'alter' INTEGER, 'erfahrung' INTEGER)");
-        
-
 	}
-	
-	private void disconnect(Connection conn){
+
+	private void disconnect(Connection conn) {
 		try {
-            if (conn != null) {
-                conn.close();
-                System.out.println("Disconnected from Database");
-            }
-        } catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
+			if (conn != null) {
+				conn.close();
+				System.out.println("Disconnected from Database");
+			}
+		} catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
-	
+
 	public ITrainer create() {
-		System.out.print("create");
-		return null;
-	}
-
-	public void delete(ITrainer trainer) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public ITrainer first() {
-		// TODO Auto-generated method stub
-		
 		doConnect();
 		Statement stmt;
 		try {
 			stmt = conn.createStatement();
-			String sql = "SELECT * FROM trainer WHERE id = 1";
+			ResultSet result = stmt.getGeneratedKeys();
+			result.next();
+			int id = result.getInt("last_insert_rowid()");
+			String sql = "SELECT * FROM trainer";
 			ResultSet rs = stmt.executeQuery(sql);
 			rs.next();
-			Trainer trainer = new Trainer(rs.getInt("id"), rs.getString("name"), rs.getInt("alter"), rs.getInt("erfahrung"));
+			Trainer trainer = new Trainer(id, "", 0, 0);
 			System.out.println(rs.getString("name"));
 			disconnect(conn);
 			return trainer;
 
 		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect(conn);
+		System.out.print("create");
+		return null;
+	}
+
+	public void delete(ITrainer trainer) {
+		doConnect();
+		try {
+			int id = trainer.getId();
+			String sql = "DELETE FROM trainer WHERE id = " + id;
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.executeUpdate();
+			System.out.println("Deleted");
+		} catch (SQLException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public ITrainer first() {
+		doConnect();
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			String sql = "SELECT * FROM trainer WHERE id =(SELECT MIN(id) FROM trainer)";
+			ResultSet rs = stmt.executeQuery(sql);
+			rs.next();
+			Trainer trainer = new Trainer(rs.getInt("id"), rs.getString("name"), rs.getInt("alter"),
+					rs.getInt("erfahrung"));
+			System.out.println(rs.getString("name"));
+			disconnect(conn);
+			return trainer;
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		disconnect(conn);
@@ -83,22 +106,93 @@ public class TrainerDaoSqlite implements ITrainerDao {
 
 	public ITrainer last() {
 		// TODO Auto-generated method stub
+		doConnect();
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			String sql = "SELECT * FROM trainer WHERE id =(SELECT MAX(id) FROM trainer)";
+			ResultSet rs = stmt.executeQuery(sql);
+			rs.next();
+			Trainer trainer = new Trainer(rs.getInt("id"), rs.getString("name"), rs.getInt("alter"),
+					rs.getInt("erfahrung"));
+			System.out.println(rs.getString("name"));
+			disconnect(conn);
+			return trainer;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect(conn);
 		return null;
 	}
 
 	public ITrainer next(ITrainer trainer) {
-		// TODO Auto-generated method stub
+		doConnect();
+		Statement stmt;
+		int cur_id = trainer.getId();
+		String sql = "SELECT * FROM trainer WHERE id =(SELECT MIN(id) FROM trainer WHERE id > 'cur_id')";
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			rs.next();
+			Trainer nextTrainer = new Trainer(rs.getInt("id"), rs.getString("name"), rs.getInt("alter"),
+					rs.getInt("erfahrung"));
+			System.out.println(rs.getString("name"));
+			disconnect(conn);
+			return nextTrainer;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect(conn);
 		return null;
 	}
 
 	public ITrainer previous(ITrainer trainer) {
 		// TODO Auto-generated method stub
+		doConnect();
+		Statement stmt;
+		int number;
+		number = trainer.getId() - 1;
+		try {
+			stmt = conn.createStatement();
+			String sql = "SELECT * FROM trainer WHERE id =" + number;
+			ResultSet rs = stmt.executeQuery(sql);
+			rs.next();
+			Trainer nextTrainer = new Trainer(rs.getInt("id"), rs.getString("name"), rs.getInt("alter"),
+					rs.getInt("erfahrung"));
+			System.out.println(rs.getString("name"));
+			disconnect(conn);
+			return nextTrainer;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		disconnect(conn);
 		return null;
 	}
 
 	public void save(ITrainer trainer) {
-		// TODO Auto-generated method stub
-		
+		int id = trainer.getId();
+		String name = trainer.getName();
+		int alter = trainer.getAlter();
+		int erfahrung = trainer.getErfahrung();
+		doConnect();
+		String sql = "INSERT INTO trainer ('id', 'name', 'alter', 'erfahrung') VALUES (?, ?, ?, ?)";
+		PreparedStatement statement;
+		try {
+			statement = conn.prepareStatement(sql);
+			statement.setInt(1, id);
+			statement.setString(2, name);
+			statement.setInt(3, alter);
+			statement.setInt(4, erfahrung);
+			statement.executeUpdate();
+			System.out.println("Update");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	public List<ITrainer> select() {
