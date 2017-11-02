74be2f125ebd533cd516f2504a04485dbdef60fe
klaus91 <32059382+klaus91@users.noreply.github.com>
2017-11-02 12:56:58 +0100

    Update SettingsManager.java

--- src/dataLayer/settings/SettingsManager.java
+++ src/dataLayer/settings/SettingsManager.java
@@ -1,14 +1,69 @@
 package dataLayer.settings;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
 public class SettingsManager {
+    private static SettingsManager instance;
+    private PersistenceSettings persistenceSettings;
 
-	public SettingsManager() {
-		// TODO Auto-generated constructor stub
-	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    private SettingsManager() {
+    }
 
-	}
+
+    public static SettingsManager getInstance() {
+        if(instance == null) {
+            instance = new SettingsManager();
+        }
+
+        return instance;
+    }
+
+
+    public PersistenceSettings getPersistenceSettings() {
+        return readPersistenceSettings();
+    }
+
+
+    private PersistenceSettings readPersistenceSettings() {
+        // todo: was muss hier getan werden?
+
+        PersistenceSettings ps = new PersistenceSettings();
+
+        try {
+            File fXmlFile = new File("config.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("persistence_type");
+            String persistenceType = nList.item(0).getTextContent();
+
+            if (persistenceType.equals("xml")){
+                ps.setPersistenceType("xml");
+            }
+            else if (persistenceType.equals("sqlite")){
+                ps.setPersistenceType("sqlite");
+            }
+
+            else {
+                throw new Exception("wrong persistence_type");
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        this.persistenceSettings = ps;
+
+        return persistenceSettings;
+    }
 
 }
