3309dfa391902e1867ca3e5142bf4653f49394f5
klaus91 <32059382+klaus91@users.noreply.github.com>
2017-11-02 13:12:38 +0100

    Update NoTrainerFoundException.java

--- src/exceptions/NoTrainerFoundException.java
+++ src/exceptions/NoTrainerFoundException.java
@@ -1,14 +1,13 @@
 package exceptions;
 
-public class NoTrainerFoundException {
+public class NoTrainerFoundException extends Exception {
 	public NoTrainerFoundException() {
 		super("NoTrainerFoundException");
 	}
-	public String NoTrainerFoundException(String a) {
-		if (a.length() == 0) {
-			return "No Trainer found";
-		}
-		return null;
-	}
-
+//	public String NoTrainerFoundException(String a) {
+//		if (a.length() == 0) {
+//			return "No Trainer found";
+//		}
+//		return null;
+//	}
 }
